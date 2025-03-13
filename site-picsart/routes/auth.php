bccfdce537bca7b7204f84653f71c003b2ae0dc5
Téo <teo@MacBook-Pro-de-Teo.local>
2025-03-13 16:04:35 +0100

    Fixed Auth

--- site-picsart/routes/auth.php
+++ site-picsart/routes/auth.php
@@ -1,25 +1,20 @@
 <?php
-/*
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
-*/
+
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
 /*
-Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
->>>>>>> adba8067093c8f8639bda6f2bff4565e93616450
+|--------------------------------------------------------------------------
+| Auth Routes
+|--------------------------------------------------------------------------
+|
+| This file contains routes related to authetification, including login,
+| and logout. These routes to corresponding controller methods that handle
+| the logicfor each action.
+|
+*/
+Route::prefix('/auth')->name('auth.')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])
         ->name('login');
 
     Route::get('/callback', [AuthController::class, 'callback'])
@@ -30,4 +25,3 @@ Route::middleware('guest')->group(function () {
             ->name('logout');
     });
 });
-*/
